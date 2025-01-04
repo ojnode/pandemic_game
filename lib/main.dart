66a1f9ef2756d2301951beb7c1e4c15251a48c87
ojnode <ojinnakabu95@gmail.com>
2025-01-04 07:39:55 +0000

    refactored code, add button widgets and functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pandemic_game/models/decision_logic.dart';
+import 'package:pandemic_game/models/economy.dart';
 import 'package:pandemic_game/models/resource.dart';
 import 'package:pandemic_game/models/population.dart';
 import 'dart:async';
@@ -35,21 +37,30 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Population greenPopulation = Population();
-  Population volPopulation = Population();
-  Resource green = Resource();
-  Resource volcanic = Resource();
+  Population redPopulation = Population();
+  Resource greenResource = Resource();
+  Resource redResource = Resource();
+  Economy greenEconomy = Economy();
+  Economy redEconomy = Economy();
   int countdownValue = 10;
   late int populationGoal;
+  late String resourceGoal;
+  DecisionTree currentTree = DecisionTree();
+  bool isTreeLoaded = false;
 
   @override
   void initState() {
     super.initState();
-    int populationEffect = green.getPopulationEffect();
-    populationGoal = greenPopulation.maintainPopulation(populationEffect);
-    Timer.periodic(const Duration(seconds: 3), (timer) {
+    int greenPopulationEffect = greenResource.getPopulationEffect();
+    int redPopulationEffect = redResource.getPopulationEffect();
+    initTree();
+    populationGoal = greenPopulation.maintainPopulation(greenPopulationEffect);
+    resourceGoal = redResource.getPriorResourceValue();
+    Timer.periodic(const Duration(seconds: 4), (timer) {
       setState(() {
         countdownValue --;
-        greenPopulation.reducePopulation(populationEffect);
+        greenPopulation.reducePopulation(greenPopulationEffect);
+        redPopulation.reducePopulation(redPopulationEffect);
       });
       if (countdownValue <= 0) {
         timer.cancel();
@@ -57,8 +68,20 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Future<void> initTree() async {
+    await currentTree.getTree('decision_tree.json');
+    isTreeLoaded = true;
+  }
+
   @override
   Widget build(BuildContext context) {
+    // used to avoid game loading before the tree is initialized
+    if (!isTreeLoaded) {
+      return Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme
@@ -71,80 +94,96 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             Row(
                 children: [
-                  Flexible(child: Column(
-                      children: [
-                        createSizeBox('assets/images/green.png'),
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            color: Colors.yellowAccent,
-                            borderRadius: BorderRadius.circular(10)
-                          ),
-                          child: Column(
-                            children: [
-                              createText( "Healthy Population:"
-                                  " ${greenPopulation.getPopulation()}",
-                                  Colors.black, 24),
-                              createText( "Resource Status: ${green.getValue()}",
-                                  Colors.black, 24),
-                          ]
-                        ),
-                      ),
-                    ]
-                    ),
-                  ),
+                  createCountryDetails(context, greenPopulation,
+                      greenResource, greenEconomy, 'assets/images/green.png'),
                   Spacer(),
                   createText(countdownValue.toString(), Colors.blue, 100),
                   Spacer(),
-                  Flexible(child: Column(
-                      children: [
-                        createSizeBox('assets/images/volcanic.png'),
-                        Container(
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                              color: Colors.yellowAccent,
-                              borderRadius: BorderRadius.circular(10)
-                          ),
-                          child: Column(
-                              children: [
-                                createText( "Healthy Population: "
-                                    "${volPopulation.getPopulation()}",
-                                    Colors.black, 24),
-                                createText( "Resource Status: "
-                                    "${volcanic.getValue()}",
-                                    Colors.black, 24),
-                              ]
-                          ),
-                        ),
-                      ]
-                    ),
-                  ),
+                  createCountryDetails(context, redPopulation, redResource,
+                      redEconomy, 'assets/images/volcanic.png'),
                 ]
             ),
-            Spacer(),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
             Container(
               padding: EdgeInsets.all(16),
               decoration: BoxDecoration(
-                  color: Colors.redAccent,
+                  color: Colors.blueGrey,
                   borderRadius: BorderRadius.circular(10)
               ),
               child: Column(
                 children: [
-                  createText( "Stop planet green's population "
+                  createText( "Stop green country population "
                       "from going less than "
                       "$populationGoal",
                       Colors.black, 24),
-                  createText( "Revert planet volcano's resources to "
-                      "${volcanic.getPriorResourceValue()}",
+                  createText( "Revert and maintain red country resources at "
+                      "$resourceGoal",
                       Colors.black, 24),
                 ],
               ),
             ),
-            Spacer(),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.05,
+            ),
+            Center(
+              child: Column(
+                children: [
+                  createText( currentTree.getCurrentDescription(),
+                      Colors.black, 24),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.08,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center, // Center vertically
+                    children: [
+                      Wrap(
+                        spacing: 16,
+                          children: [
+                            for (var entry in
+                            currentTree.getCurrentChoices().entries)
+                              ElevatedButton(
+                                onPressed: () {
+                                  try {
+                                    setState(() {
+                                      bool resourceCheck = redResource
+                                          .checkResourceGoal();
+                                      // revert back to nodeA if player is to far from goal
+                                      if (currentTree.isNodeGreaterThan(
+                                          entry.value,
+                                          resourceCheck)) {
+                                        currentTree.setCurrentNode("nodeA");
+                                      } else {
+                                        currentTree.setCurrentNode(entry.value);
+                                      }
+
+                                      updateCountry(currentTree, "redresources",
+                                          "redEconomy", redPopulation, redResource,
+                                          redEconomy);
+                                      updateCountry(currentTree, "greenresources",
+                                          "greenEconomy", greenPopulation, greenResource,
+                                          greenEconomy);
+                                    });
+                                  } catch (e) {
+                                    print("Error button login: $e");
+                                  }
+                                },
+                                child: createText(entry.key, Colors.black, 25),
+                              ),
+                        ],
+                      ),
+                    ]
+                  ),
+                ]
+              ),
+            ),
+            Spacer()
           ]
       ),
     );
   }
+
 }
 
 // do this with a class later
@@ -168,3 +207,50 @@ Text createText(String inputText, Color color, double font) {
     ),
   );
 }
+
+// create individual country's details
+Flexible createCountryDetails(context, population, resource, economy, imagePath) {
+  return Flexible(child: Column(
+      children: [
+        createSizeBox(imagePath),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.02,
+        ),
+        Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: Colors.blueGrey,
+              borderRadius: BorderRadius.circular(10)
+          ),
+          child: Column(
+              children: [
+                createText( "Healthy Population:"
+                    " ${population.getPopulation()}",
+                    Colors.black, 24),
+                createText( "Resource Status: ${resource.getValue()}",
+                    Colors.black, 24),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                Column(
+                    children: [
+                      createText("Economy", Colors.black, 20),
+                      LinearProgressIndicator(
+                        value: economy.getEconomyLevel() / 100,
+                        backgroundColor: Colors.grey[300],
+                        color: economy.getEconomyLevel() > 75 ?
+                        Colors.green : economy.getEconomyLevel() > 50 ?
+                        Colors.yellow : Colors.red,
+                        minHeight: 20,
+                      )
+                    ]
+                )
+              ]
+          ),
+        ),
+      ]
+    ),
+  );
+}
+
+
